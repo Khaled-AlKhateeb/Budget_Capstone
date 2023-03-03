e073cf65e83d2fc73bb7c21f1eaf7db9fb74b0c8
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2023-03-03 19:46:53 +0300

    Fix linters

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,13 @@
 require_relative "boot"
 require 'dotenv/load'
 
-
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module BudgetApp
+module BudgetControl
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
